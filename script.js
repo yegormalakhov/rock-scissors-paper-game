7b2617df67002ab38ca05ee9c1d70dee91875259
ymalakhov <it.malakhov@gmail.com>
2022-03-14 17:20:59 +0100

    Output of userChoice to html

--- script.js
+++ script.js
@@ -1,35 +1,36 @@
 let userInput;
 const userChoices = document.querySelectorAll(".choices");
 const start = document.querySelector("#start");
+const userChoice = document.querySelector("#userChoice");
+const resultOutput = document.querySelector('#result');
+
 
 userChoices.forEach((choice) => {
   choice.addEventListener("click", (e) => {
     userInput = e.target.id;
+    userChoice.textContent = `You chose ${userInput}`;
     console.log(userInput);
   });
 });
 
 start.addEventListener("click", () => console.log("something"));
 
-
 // getUserChoice('paper');
-const getComputerChoice = () => {
-  let number = Math.floor(Math.random() * 3);
-  switch (number) {
-    case 0:
-      return "rock";
-    case 1:
-      return "paper";
-    case 2:
-      return "scissors";
-  }
-};
+// const getComputerChoice = () => {
+//   let number = Math.floor(Math.random() * 3);
+//   switch (number) {
+//     case 0:
+//       return "rock";
+//     case 1:
+//       return "paper";
+//     case 2:
+//       return "scissors";
+//   }
+// };
 // console.log(getComputerChoice());
 
 //   const determineWinner = (userChoice, computerChoice) => {
-//   if(userChoice === 'bomb'){
-//     return "You won";
-//   }
+  
 //   if (userChoice === computerChoice){
 //     return "The game is a tie!";
 //       }
@@ -60,14 +61,13 @@ const getComputerChoice = () => {
 //   // console.log(determineWinner('rock', 'scissors'));
 
 //   const playGame = () => {
-//   const userChoice = getUserChoice('bomb');
+// //   const userChoice = getUserChoice('bomb');
 //   const computerChoice = getComputerChoice();
-//   console.log("You threw: " + userChoice);
-//   console.log("Computer threw: " + computerChoice);
+// //   console.log("You threw: " + userChoice);
+// //   console.log("Computer threw: " + computerChoice);
 //   console.log(determineWinner(userChoice, computerChoice));
 //   }
 //   playGame();
-
 
 // NOT needed original
 
@@ -78,4 +78,9 @@ const getComputerChoice = () => {
 //   } else {
 //     console.log("Please give a correct option");
 //   }
+// }
+
+
+// if(userChoice === 'bomb'){
+//   return "You won";
 // }
