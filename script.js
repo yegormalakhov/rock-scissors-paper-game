c46f03a4be1d285d509ba5fa811c186dafc1b7c7
ymalakhov <it.malakhov@gmail.com>
2022-03-14 18:02:44 +0100

    working comp choice to html. save in variable work

--- script.js
+++ script.js
@@ -2,35 +2,42 @@ let userInput;
 const userChoices = document.querySelectorAll(".choices");
 const start = document.querySelector("#start");
 const userChoice = document.querySelector("#userChoice");
-const resultOutput = document.querySelector('#result');
-
+const resultOutput = document.querySelector("#result");
+const compChoice = document.querySelector("#compChoice");
 
 userChoices.forEach((choice) => {
   choice.addEventListener("click", (e) => {
     userInput = e.target.id;
     userChoice.textContent = `You chose ${userInput}`;
-    console.log(userInput);
+    compChoice.textContent = `Computer chose ${computerChoice}`;
+    console.log(`you threw ${userInput}`);
+    console.log(`Computer threw ${computerChoice}`);
   });
 });
 
-start.addEventListener("click", () => console.log("something"));
+start.addEventListener("click", () => {
+  userChoice.textContent = "";
+  userInput = "";
+  compChoice.textContent = "";
+  playGame();
+});
 
-// getUserChoice('paper');
-// const getComputerChoice = () => {
-//   let number = Math.floor(Math.random() * 3);
-//   switch (number) {
-//     case 0:
-//       return "rock";
-//     case 1:
-//       return "paper";
-//     case 2:
-//       return "scissors";
-//   }
-// };
+// getUserChoice("paper");
+const getComputerChoice = () => {
+  let number = Math.floor(Math.random() * 3);
+  switch (number) {
+    case 0:
+      return "rock";
+    case 1:
+      return "paper";
+    case 2:
+      return "scissors";
+  }
+};
 // console.log(getComputerChoice());
 
 //   const determineWinner = (userChoice, computerChoice) => {
-  
+
 //   if (userChoice === computerChoice){
 //     return "The game is a tie!";
 //       }
@@ -59,14 +66,13 @@ start.addEventListener("click", () => console.log("something"));
 // }
 
 //   // console.log(determineWinner('rock', 'scissors'));
-
-//   const playGame = () => {
-// //   const userChoice = getUserChoice('bomb');
-//   const computerChoice = getComputerChoice();
-// //   console.log("You threw: " + userChoice);
-// //   console.log("Computer threw: " + computerChoice);
-//   console.log(determineWinner(userChoice, computerChoice));
-//   }
+let computerChoice;
+const playGame = () => {
+  computerChoice = getComputerChoice();
+  // //   console.log("You threw: " + userChoice);
+  console.log("Computer threw: " + computerChoice);
+  //   console.log(determineWinner(userChoice, computerChoice));
+};
 //   playGame();
 
 // NOT needed original
@@ -80,7 +86,8 @@ start.addEventListener("click", () => console.log("something"));
 //   }
 // }
 
-
 // if(userChoice === 'bomb'){
 //   return "You won";
 // }
+// from const playGame
+// // //   const userChoice = getUserChoice('bomb');
