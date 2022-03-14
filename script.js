6db80382144d1036f83ddd844f18a2a0977920b4
ymalakhov <it.malakhov@gmail.com>
2022-03-14 16:59:57 +0100

    created start button and working event for it

--- script.js
+++ script.js
@@ -1,15 +1,75 @@
-const userChoices = document.querySelectorAll('.choices');
-// console.log(userChoices);
-let userInput = undefined;
+let userInput;
+const userChoices = document.querySelectorAll(".choices");
+const start = document.querySelector("#start");
 
-userChoices.forEach(choice => {
-  choice.addEventListener('click', (e) => {
-  e.stopPropagation();
-  userInput = e.target.id;
-  console.log(userInput);
-  })
-})
+userChoices.forEach((choice) => {
+  choice.addEventListener("click", (e) => {
+    userInput = e.target.id;
+    console.log(userInput);
+  });
+});
 
+start.addEventListener("click", () => console.log("something"));
+
+
+// getUserChoice('paper');
+const getComputerChoice = () => {
+  let number = Math.floor(Math.random() * 3);
+  switch (number) {
+    case 0:
+      return "rock";
+    case 1:
+      return "paper";
+    case 2:
+      return "scissors";
+  }
+};
+// console.log(getComputerChoice());
+
+//   const determineWinner = (userChoice, computerChoice) => {
+//   if(userChoice === 'bomb'){
+//     return "You won";
+//   }
+//   if (userChoice === computerChoice){
+//     return "The game is a tie!";
+//       }
+//   if ( userChoice === 'rock'){
+//     if (computerChoice === 'paper'){
+//     return "Computer won";
+//     } else {
+//     return 'Player won';
+//     }
+//   }
+//    if ( userChoice === 'paper'){
+//     if (computerChoice === 'scissors'){
+//       return "Computer won";
+//     } else {
+//       return 'Player won';
+//     }
+//   }
+//   if (userChoice === 'scissors') {
+//     if (computerChoice === 'rock'){
+//       return "Computer won";
+//     } else {
+//       return 'Player won';
+//     }
+
+//   }
+// }
+
+//   // console.log(determineWinner('rock', 'scissors'));
+
+//   const playGame = () => {
+//   const userChoice = getUserChoice('bomb');
+//   const computerChoice = getComputerChoice();
+//   console.log("You threw: " + userChoice);
+//   console.log("Computer threw: " + computerChoice);
+//   console.log(determineWinner(userChoice, computerChoice));
+//   }
+//   playGame();
+
+
+// NOT needed original
 
 // function getUserChoice(userInput) {
 //   userInput = userInput.toLowerCase();
@@ -19,63 +79,3 @@ userChoices.forEach(choice => {
 //     console.log("Please give a correct option");
 //   }
 // }
-  
-// getUserChoice('paper');
-const getComputerChoice = () => {
-   let number = Math.floor(Math.random()*3);
-    switch (number){
-      case 0:
-        return 'rock';
-      case 1:
-        return 'paper';
-      case 2:
-        return 'scissors';
-    }
-  }
-  // console.log(getComputerChoice());
-  
-//   const determineWinner = (userChoice, computerChoice) => {
-//   if(userChoice === 'bomb'){
-//     return "You won";
-//   }
-//   if (userChoice === computerChoice){
-//     return "The game is a tie!";
-//       } 
-//   if ( userChoice === 'rock'){
-//     if (computerChoice === 'paper'){
-//     return "Computer won";
-//     } else {
-//     return 'Player won';
-//     } 
-//   }
-//    if ( userChoice === 'paper'){
-//     if (computerChoice === 'scissors'){
-//       return "Computer won";
-//     } else {
-//       return 'Player won';
-//     }
-//   } 
-//   if (userChoice === 'scissors') {
-//     if (computerChoice === 'rock'){
-//       return "Computer won";
-//     } else {
-//       return 'Player won';
-//     }
-  
-//   }
-// }
-  
-//   // console.log(determineWinner('rock', 'scissors'));
-  
-//   const playGame = () => {
-//   const userChoice = getUserChoice('bomb');
-//   const computerChoice = getComputerChoice();
-//   console.log("You threw: " + userChoice);
-//   console.log("Computer threw: " + computerChoice);
-//   console.log(determineWinner(userChoice, computerChoice));
-//   }
-//   playGame();
-  
-  
-  
-  
